459c0a450fc79c17a9f6b211fc87f8cbdbbe7e0f
Marco Floriano <marcofloriano@gmail.com>
2025-12-02 10:29:58 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from '/src/components/Header'
+import Entry from '/src/components/Entry'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Entry />
+      <Entry />
+    </>
+  )
+}
